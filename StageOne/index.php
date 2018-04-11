e733e4fd3f03fe334c79c96a99bfbaf8b6f65221
vanessaozogu <vanessaozogu@gmail.com>
2018-04-11 11:47:33 +0100

    First commit

---
+++ StageOne/index.php
@@ -0,0 +1,37 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <!-- Title goes here -->
+    <title>HNG INTERNSHIP - STAGE ONE</title>
+
+    <!-- Stylesheet Information -->
+    <link rel="stylesheet" href="intern.css" type="text/css">
+
+    <!-- Font Used -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  </head>
+  <body>
+    <header>
+      <!-- Header information -->
+      <div class="row">
+        <!-- Header Logo information -->
+        <div class="logo">
+          <img src="images/realifotta.png" alt="">
+        </div> <!-- Header Logo info ends -->
+        <!-- Nav Links -->
+        <ul class="main-nav">
+          <li><a href="">HOME</a></li>
+          <li><a href="">SIGN UP</a></li>
+          <li><a href="">CONTACT US</a></li>
+        </ul><!-- Nav Links info ends here -->
+      </div><!-- Header info ends here -->
+
+      <div class="hero">
+        <h1>ACCELERATE YOUR FUTURE</h1>
+        <h4 margin-top="5px">Hello Vanessa, the time is <?php echo date("l jS \of F Y h:i:s A") ; ?> </h4>
+      </div>
+    </header>
+  </body>
+</html>
